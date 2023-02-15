1cc804fb6dafa11b75819e9173f3e4f2a16d7c40
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2023-02-15 16:32:58 -0300

    Adicionando mock ao teste de createUser

--- src/user/createUser.spec.js
+++ src/user/createUser.spec.js
@@ -1,65 +1,81 @@
 import { createUser } from "../user/createUser.js";
 
-let usersInMemory = [];
+const testUser = {
+    email: 'teste@email.com',
+    name: 'Jhon Doe',
+    picture: 'https://www.euax.com.br/wp-content/uploads/2019/10/Teste.png',
+    username: 'usertest'
+};
 
-describe("Criar Usuario", ()=> {
-    
-    it("Deve criar um usuario se todos os campos forem enviados", async ()=>{
-        let user = await createUser({ 
-         email: 'teste@email.com',
-         name: 'Jhon Doe',
-         picture: 'https://www.euax.com.br/wp-content/uploads/2019/10/Teste.png',
-         username: 'usertest' 
-        }, true);
+const mockDbUserCreate = jest.fn().mockImplementation(() => {
+    return {
+        id: 123,
+        ...testUser
+    };
+});
 
-        usersInMemory.push(user);
+jest.mock("../database/db.js", () => {
+    return {
+        user: {
+            create: mockDbUserCreate
+        }
+    }
+});
+
+describe("Criar Usuario", () => {
+
+    it("Deve criar um usuario se todos os campos forem enviados", async () => {
+        let user = await createUser(testUser);
 
         //Verifica se o usuario foi criado, se foi ele deve ter um id
-        expect(user).toHaveProperty("id")
-    })  
+        expect(user).toHaveProperty("id");
+        expect(mockDbUserCreate).toHaveBeenCalledWith({
+            data: testUser,
+        });
+    });
 
-    it("Não deve criar um usuario se o campo name não for enviado", async ()=>{
+    it("Não deve criar um usuario se o campo name não for enviado", async () => {
 
-        expect(async()=> {
-            await createUser({ 
+        expect(async () => {
+            await createUser({
                 email: 'teste@email.com',
                 picture: 'https://www.euax.com.br/wp-content/uploads/2019/10/Teste.png',
-                username: 'usertest' 
-               }, true);
+                username: 'usertest'
+            }, true);
         }).rejects.toBeInstanceOf(Error)
-    })  
+    })
 
-    it("Não deve criar um usuario se o campo email não for enviado", async ()=>{
+    it("Não deve criar um usuario se o campo email não for enviado", async () => {
 
-        expect(async()=> {
-            await createUser({ 
+        expect(async () => {
+            await createUser({
                 name: 'Jhon Doe',
                 picture: 'https://www.euax.com.br/wp-content/uploads/2019/10/Teste.png',
-                username: 'usertest' 
-               }, true);
+                username: 'usertest'
+            }, true);
         }).rejects.toBeInstanceOf(Error)
-    })  
+    })
 
-    it("Não deve criar um usuario se o campo username não for enviado", async ()=>{
+    it("Não deve criar um usuario se o campo username não for enviado", async () => {
 
-        expect(async()=> {
-            await createUser({ 
+        expect(async () => {
+            await createUser({
                 name: 'Jhon Doe',
                 picture: 'https://www.euax.com.br/wp-content/uploads/2019/10/Teste.png',
-                email: 'teste@email.com' 
-               }, true);
+                email: 'teste@email.com'
+            }, true);
         }).rejects.toBeInstanceOf(Error)
-    })  
+    })
 
-    it("Não deve criar um usuario se o campo picture não for enviado", async ()=>{
+    it("Não deve criar um usuario se o campo picture não for enviado", async () => {
 
-        expect(async()=> {
-            await createUser({ 
+        expect(async () => {
+            await createUser({
                 name: 'Jhon Doe',
                 username: 'usertest',
-                email: 'teste@email.com' 
-               }, true);
+                email: 'teste@email.com'
+            }, true);
         }).rejects.toBeInstanceOf(Error)
-    })  
-    
+    })
+
 })
